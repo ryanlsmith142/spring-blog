43a8af9ee56901ea7ab3f2aceb1ed7a4ec661eda
Ryan Smith <ryan.l.smith142@gmail.com>
2019-11-04 15:54:38 -0600

    add math and home controller

---
+++ src/main/java/com/codeup/blog/blog/controllers/MathController.java
@@ -0,0 +1,42 @@
+package com.codeup.blog.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class MathController {
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String add(@PathVariable int num1, @PathVariable int num2) {
+        int total = num1 + num2;
+
+        return Integer.toString(total);
+    }
+
+    @GetMapping("/subtract/{num1}/and/{num2}")
+    @ResponseBody
+    public String subtract(@PathVariable int num1, @PathVariable int num2) {
+        int total = num1 - num2;
+
+        return Integer.toString(total);
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply(@PathVariable int num1, @PathVariable int num2) {
+        int total = num1 * num2;
+
+        return Integer.toString(total);
+    }
+
+    @GetMapping("/divide/{num1}/and/{num2}")
+    @ResponseBody
+    public String divide(@PathVariable int num1, @PathVariable int num2) {
+        int total = num1 / num2;
+
+        return Integer.toString(total);
+    }
+}
