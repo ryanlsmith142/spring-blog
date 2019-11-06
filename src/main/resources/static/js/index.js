92b2dddfe6f264714cccf83415ddab6869d42baf
Ryan Smith <ryan.l.smith142@gmail.com>
2019-11-06 14:54:41 -0600

    feat: modal pops up

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,20 @@
+const editMovieClickEvent = () => {
+
+    //returns an HTML collection of all edit-buttons on page
+    let editButtons = document.getElementsByClassName("openModal");
+
+    //loops through html collection stored in editButtons var and adds an open modal event listener.
+    for(let i = 0; i < editButtons.length; i++) {
+
+            editButtons[i].addEventListener("click", function() {
+
+            document.getElementById('modal').style.display = "block";
+
+            let currentMovieId = editButtons[i].id;
+            console.log(currentMovieId);
+            return currentMovieId;
+        }); // event listener
+    } //for
+}; //editMovieClickEvent()
+
+editMovieClickEvent();
