e23fc07d8745024b1a59eb4d158548865634ec89
Ryan Smith <ryan.l.smith142@gmail.com>
2019-11-07 10:29:36 -0600

    add PostDetails Model

---
+++ src/main/java/com/codeup/blog/blog/models/PostDetails.java
@@ -0,0 +1,61 @@
+package com.codeup.blog.blog.models;
+
+import javax.persistence.*;
+
+@Entity
+public class PostDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, columnDefinition"int(11) UNSIGNED")
+    private long id;
+
+    private boolean isAwesome;
+
+    @Column(columnDefinition =  "TEXT")
+    private String historyOfPost;
+
+    @Column(length = 255)
+    private String topicDescription;
+
+    public PostDetails(){}
+
+    public PostDetails(long id, boolean isAwesome, String historyOfPost, String topicDescription) {
+        this.id = id;
+        this.isAwesome = isAwesome;
+        this.historyOfPost = historyOfPost;
+        this.topicDescription = topicDescription;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isAwesome() {
+        return isAwesome;
+    }
+
+    public void setAwesome(boolean awesome) {
+        isAwesome = awesome;
+    }
+
+    public String getHistoryOfPost() {
+        return historyOfPost;
+    }
+
+    public void setHistoryOfPost(String historyOfPost) {
+        this.historyOfPost = historyOfPost;
+    }
+
+    public String getTopicDescription() {
+        return topicDescription;
+    }
+
+    public void setTopicDescription(String topicDescription) {
+        this.topicDescription = topicDescription;
+    }
+}
