88e0b9b6bee20859b02d1bcaa6628bda28dab79e
Ryan Smith <ryan.l.smith142@gmail.com>
2019-11-12 11:46:40 -0600

    fix edit feature

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,28 +1,30 @@
-const editMovieClickEvent = () => {
-
-    //returns an HTML collection of all edit-buttons on page
-    let editButtons = document.getElementsByClassName("openModal");
-
-    //loops through html collection stored in editButtons var and adds an open modal event listener.
-    for(let i = 0; i < editButtons.length; i++) {
-
-            editButtons[i].addEventListener("click", function() {
-
-            document.getElementById('modal').style.display = "block";
-
-            let currentMovieId = editButtons[i].id;
 
 
-            let hiddenInput = `<input type="hidden" name="id" value=${currentMovieId}>`;
+            document.getElementById("openModal").addEventListener("click", function() {
 
-            document.getElementById('modalForm').setAttribute("action", `/posts/${currentMovieId}/edit`);
+                document.getElementById('modal').style.display = "block";
 
-            document.getElementById("saveChanges").innerHTML = hiddenInput;
+                let currentMovieId = document.getElementById("postId").value;
 
-            return currentMovieId;
+                let titleValue = document.getElementById("individualTitle").textContent;
+
+                let bodyValue = document.getElementById("individualBody").textContent;
+
+                let hiddenInput = `<input type="hidden" name="id" value=${currentMovieId}>`;
+
+                document.getElementById('modalTitle').value = titleValue;
+
+                document.getElementById('modalBody').value = bodyValue;
+
+                document.getElementById('modalForm').setAttribute("action", `/posts/${currentMovieId}/edit`);
+
+                document.getElementById("saveChanges").innerHTML = hiddenInput;
+
+                return currentMovieId;
+
         }); // event listener
-    } //for
-}; //editMovieClickEvent()
 
-editMovieClickEvent();
+
+
+
 
