93acfa7415be2e2fc17bdbf17699f2503df717ee
Ryan Smith <ryan.l.smith142@gmail.com>
2019-11-13 10:58:20 -0600

    add UserWithRoles Model;

--- src/main/java/com/codeup/blog/blog/models/User.java
+++ src/main/java/com/codeup/blog/blog/models/User.java
@@ -34,6 +34,13 @@ public class User {
         this.password = password;
     }
 
+    public User(User copy) {
+        id = copy.id;
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
     public long getId() {
         return id;
     }
