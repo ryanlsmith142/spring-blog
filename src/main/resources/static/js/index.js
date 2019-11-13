8fc725078b3b0363fa1163e1e513df768177731f
Ryan Smith <ryan.l.smith142@gmail.com>
2019-11-13 15:09:49 -0600

    only the author of a post can see the delete and edit buttons

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,12 +3,17 @@ document.getElementById("openModal").addEventListener("click", function() {
     document.getElementById('modal').style.display = "block";
 
     let currentPostId = document.getElementById("postId").value;
+
     let titleValue = document.getElementById("individualTitle").textContent;
     let bodyValue = document.getElementById("individualBody").textContent;
+
     let hiddenInput = `<input type="hidden" name="id" value=${currentPostId}>`;
+
     document.getElementById('modalTitle').value = titleValue;
     document.getElementById('modalBody').value = bodyValue;
+
     document.getElementById('modalForm').setAttribute("action", `/posts/${currentPostId}/edit`);
+
     document.getElementById("saveChanges").innerHTML = hiddenInput;
     return currentPostId;
 }); // event listener
