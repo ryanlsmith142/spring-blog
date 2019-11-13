1c7cb5a7eb10f4d0e50c100df8587c146e5d3eab
Ryan Smith <ryan.l.smith142@gmail.com>
2019-11-13 11:31:07 -0600

    add UserController

---
+++ src/main/java/com/codeup/blog/blog/controllers/UserController.java
@@ -0,0 +1,37 @@
+package com.codeup.blog.blog.controllers;
+
+import com.codeup.blog.blog.dao.UserRepository;
+import com.codeup.blog.blog.models.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    private UserRepository userDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model vModel) {
+        vModel.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
+    }
+
+
+}
